8dd5d967595db03e74a663ffaf556937c8ba1983
Maxwell Easter <maxwelleaster00@gmail.com>
2025-08-18 07:19:26 +0100

    add desc.js

---
+++ desc.js
@@ -0,0 +1,16 @@
+
+/*
+
+core features:
+  - block websites
+  - whitelist
+  - overlay
+  - schedule / time-based blocking
+  - premium "block all except whitelist"
+  - simple ui
+  - lightweight & fast
+  - overlay theme 
+  - toggle blocker on/off
+  - snooze duration 
+
+*/
